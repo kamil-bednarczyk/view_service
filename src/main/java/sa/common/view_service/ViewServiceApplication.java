45da0b77a661a2105686063b1b8eb5d57fc6375f
Bednarczyk <klby@gft.com>
2018-05-28 15:35:41 +0200

    setup repo

---
+++ src/main/java/sa/common/view_service/ViewServiceApplication.java
@@ -0,0 +1,12 @@
+package sa.common.view_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViewServiceApplication.class, args);
+	}
+}
